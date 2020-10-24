951f7cd9fcfb815bbc06ef2a611b6ebd5a4a5532
Andrew Goacher <goach@hey.com>
2020-10-24 15:29:40 +0100

    Initial commit

---
+++ Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Game.Core;
+
+namespace Game
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
